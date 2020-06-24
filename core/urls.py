6afec4d20e66320bf88517c4986afb3b8459889b
Damian Ly <lydamian@gmail.com>
2020-06-23 18:13:42 -0700

    added new templating files for better modularity and readability

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import item_list
+from .views import item_list, home_page, product_page, checkout_page
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name='item-list')
+    path('', home_page, name='home'),
+    path('checkout/', checkout_page, name='checkout'),
+    path('product/', product_page, name='product')
 ]
--- core/views.py
+++ core/views.py
@@ -7,4 +7,25 @@ def item_list(request):
     context = {
         'items': Item.objects.all()
     }
+    return render(request, "item-list.html", context)
+
+
+def home_page(request):
+    context = {
+        'items': Item.objects.all()
+    }
     return render(request, "home-page.html", context)
+
+
+def checkout_page(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, "checkout-page.html", context)
+
+
+def product_page(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, "product-page.html", context)
