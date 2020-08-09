1229632da067ce2984d6252b5b89e8e239f8890f
Damian Ly <lydamian@gmail.com>
2020-08-09 02:16:01 -0700

    Added add to cart feature, created view stub for the delete from cart view

--- core/models.py
+++ core/models.py
@@ -33,12 +33,21 @@ class Item(models.Model):
             'slug': self.slug
         })
 
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
 
 class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.title
+        return f"{self.quantity} of {self.item.title}"
 
 
 class Order(models.Model):
@@ -50,7 +59,7 @@ class Order(models.Model):
     ordered = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
+        return self.user.username
 
 
 def add_to_cart(request, slug):
--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from .views import products, checkout, HomeView, ItemDetailView
+from .views import (
+    products,
+    checkout,
+    HomeView,
+    ItemDetailView,
+    add_to_cart, remove_from_cart
+
+)
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('checkout/', checkout, name='checkout'),
-    path('product/<slug>', ItemDetailView.as_view(), name='product')
+    path('product/<slug>', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>', add_to_cart, name='add-to-cart')
+    path('remove-from-cart/<slug>', remove_from_cart, name='remove-from-cart')
 ]
